2fd5ed6425a3d69f8923aeb231b34d8b02abb6e7
nogalavi <noga.lavi1@gmail.com>
2016-04-09 14:40:19 +0300

    Matrix class - added rotation matrix creator

--- src/RayTracing/Matrix.java
+++ src/RayTracing/Matrix.java
@@ -14,6 +14,64 @@ public class Matrix {
         int n = array[0].length;
     }
 
+    public static Matrix createRotationMatrix(double xAngle, double yAngle, double zAngle) {
+        Matrix xRotation = createXrotationMatrix(xAngle);
+        Matrix yRotation = createYrotationMatrix(yAngle);
+        Matrix zRotation = createZrotationMatrix(zAngle);
+        Matrix xByY = xRotation.multiplyByMatrix(yRotation);
+        Matrix result = xByY.multiplyByMatrix(zRotation);
+        return result;
+
+    }
+
+    private static Matrix createXrotationMatrix(double xAngle) {
+        float cosX = (float) Math.cos(Math.toRadians(xAngle));
+        float sinX = (float) Math.sin(Math.toRadians(xAngle));
+        float[][] array = new float[3][3];
+        array[0][0] = 1;
+        array[0][1] = 0;
+        array[0][2] = 0;
+        array[1][0] = 0;
+        array[1][1] = cosX;
+        array[1][2] = -1 * sinX;
+        array[2][0] = 0;
+        array[2][1] = sinX;
+        array[2][2] = cosX;
+        return new Matrix(array);
+    }
+
+    private static Matrix createYrotationMatrix(double yAngle) {
+        float cosY = (float) Math.cos(Math.toRadians(yAngle));
+        float sinY = (float) Math.sin(Math.toRadians(yAngle));
+        float[][] array = new float[3][3];
+        array[0][0] = cosY;
+        array[0][1] = 0;
+        array[0][2] = sinY;
+        array[1][0] = 0;
+        array[1][1] = 1;
+        array[1][2] = 0;
+        array[2][0] = -1 * sinY;
+        array[2][1] = 0;
+        array[2][2] = cosY;
+        return new Matrix(array);
+    }
+
+    private static Matrix createZrotationMatrix(double zAngle) {
+        float cosZ = (float) Math.cos(Math.toRadians(zAngle));
+        float sinZ = (float) Math.sin(Math.toRadians(zAngle));
+        float[][] array = new float[3][3];
+        array[0][0] = cosZ;
+        array[0][1] = -1 * sinZ;
+        array[0][2] = 0;
+        array[1][0] = sinZ;
+        array[1][1] = cosZ;
+        array[1][2] = 0;
+        array[2][0] = 0;
+        array[2][1] = 0;
+        array[2][2] = 1;
+        return new Matrix(array);
+    }
+
     // return C = A * B
     public Matrix multiplyByMatrix(Matrix other) {
         float[][] A = this.array;
